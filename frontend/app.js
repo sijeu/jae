c39516334869e726300a728c0919ea7d61f2a139
sijeu <charlesjuliannvaldez22@gmail.com>
2026-09-25 00:10:02 +0800

    Update API_URL to new endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://charles-8khr.onrender.com";
+const API_URL = "https://jae-inky.vercel.app";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
