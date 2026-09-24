3248cce8a6fb893a1c4dba7ffd92c2e0bbf90f47
sijeu <charlesjuliannvaldez22@gmail.com>
2026-09-25 00:19:06 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://jae-inky.vercel.app/";
+const API_URL = "https://jae-61fb.onrender.com";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
